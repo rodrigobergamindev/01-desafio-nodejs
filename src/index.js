f06d11f10d45c47a60202a05cfb3ff008a49116e
Rodrigo <rodrigobergamindev@gmail.com>
2021-05-11 11:59:44 -0300

    implementando retorno métodos http

--- src/index.js
+++ src/index.js
@@ -1,33 +1,82 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(customer => customer.username === username)
+
+  if(!user) {
+    return response.status(400).json({error: "User not found"})
+}
+
+request.customer = user;
+
+return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  const id = uuidv4();
+
+  const UserAlreadyExists = users.some((user) => user.username === username)
+
+  if(UserAlreadyExists) {
+    return response.status(400).json({error: "User already exists!"})
+  }
+
+  users.push({
+    name,
+    username,
+    id,
+    todos: []
+  })
+
+  return response.status(201).send("User Created")
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
+    const {user} = request;
+    return response.status(201).json(user.todos)
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
+  const {title, deadline} = request.body;
+  const {user} = request;
+
+  const dateFormat = new Date(deadline + " 00:00")
+  
+  const todo = {
+    title,
+    deadline: new Date(dateFormat).toDateString(),
+    done: false,
+    id: uuidv4(),
+    created_at: new Date().toDateString()
+  }
+
+  users.todos.push(todo)
+  return response.status(201).send()
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+    const { title, deadline } = request.body;
+    const { user } = request;
+
+    //Realizar alteração no title e deadline do todo cujo ID está nos parâmetros da rota
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
